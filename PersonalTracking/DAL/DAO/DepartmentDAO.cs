d4c0956a4df73116b7728791ff5b77cd7ec7d871
Ruben <rdfortu@gmail.com>
2021-06-28 14:41:29 +0200

    Conectar capas y crear Department

---
+++ PersonalTracking/DAL/DAO/DepartmentDAO.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.DAO
+{
+    public class DepartmentDAO : EmployeeContext
+    {
+        /// <summary>
+        /// Se agrega la entidad department a la tabla DEPARTMENTE
+        /// Para finalizar la insercción db.SubmitChanges();
+        /// </summary>
+        /// <param name="department"></param>
+        public static void AddDepartmentDAO(DEPARTMENT department)
+        {
+            try
+            {
+                db.DEPARTMENT.InsertOnSubmit(department);
+                db.SubmitChanges();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
--- PersonalTracking/DAL/DAO/EmployeeContext.cs
+++ PersonalTracking/DAL/DAO/EmployeeContext.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace DAL.DAO
 {
-    class EmployeeContext
+    public class EmployeeContext
     {
         public static EmployeeDataClassDataContext db = new EmployeeDataClassDataContext();
     }
