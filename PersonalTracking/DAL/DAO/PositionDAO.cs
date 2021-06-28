ed751bdfa1b45d724f2ccf70b8d3ac77875be3b1
Ruben <rdfortu@gmail.com>
2021-06-28 15:39:52 +0200

    Añadir puestos de trabajo(Conectar capas)

---
+++ PersonalTracking/DAL/DAO/PositionDAO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.DAO
+{
+    public class PositionDAO : EmployeeContext
+    {
+        public static void AddPosition(POSITION position)
+        {
+            try
+            {
+                db.POSITION.InsertOnSubmit(position);
+                db.SubmitChanges();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
