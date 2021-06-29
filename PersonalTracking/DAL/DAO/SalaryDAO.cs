1e28aa2fb908239fda436cc4fdd1883722e8a99d
Ruben <rdfortu@gmail.com>
2021-06-29 11:39:36 +0200

    Listar Salarios => En DAO genero la lista

--- PersonalTracking/DAL/DAO/SalaryDAO.cs
+++ PersonalTracking/DAL/DAO/SalaryDAO.cs
@@ -30,7 +30,44 @@ namespace DAL.DAO
 
         public static List<SalaryDetailDTO> GetSalaies()
         {
-            throw new NotImplementedException();
+            List<SalaryDetailDTO> salaryList = new List<SalaryDetailDTO>();
+            var list = (from s in db.SALARY
+                        join e in db.EMPLOYEE on s.EmployeeID equals e.ID
+                        join m in db.MONTHS on s.MonthID equals m.ID
+                        select new
+                        {
+                            userNo = e.UserNo,
+                            name = e.Name,
+                            surname = e.SurName,
+                            employeeID = s.EmployeeID,
+                            amount = s.Amount,
+                            year = s.Year,
+                            monthName = m.MonthName,
+                            monthID = s.MonthID,
+                            salaryID = s.ID,
+                            departmentID = e.DepartmentID,
+                            positionID = e.PositionID
+                        }).OrderBy(x => x.year).ToList();
+
+            foreach (var item in list)
+            {
+                SalaryDetailDTO dto = new SalaryDetailDTO();
+                dto.userNo = item.userNo;
+                dto.name = item.name;
+                dto.surname = item.surname;
+                dto.employeeID = item.employeeID;
+                dto.salaryAmount = item.amount;
+                dto.salaryYear = item.year;
+                dto.monthName = item.monthName;
+                dto.monthID = item.monthID;
+                dto.salaryID = item.salaryID;
+                dto.departmentID = item.departmentID;
+                dto.positionID = item.positionID;
+                dto.oldSalary = item.amount;
+                salaryList.Add(dto);
+            }
+
+            return salaryList;
         }
     }
 }
