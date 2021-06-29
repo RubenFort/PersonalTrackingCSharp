0034b84a6f9725aae7e396e67216730ea2b208fe
Ruben <rdfortu@gmail.com>
2021-06-29 03:10:03 +0200

    Añadir Tarea => Mostrar información recuperada en dataGridView y en los ComboBox

--- PersonalTracking/PersonalTracking/FrmTask.cs
+++ PersonalTracking/PersonalTracking/FrmTask.cs
@@ -30,12 +30,39 @@ namespace PersonalTracking
         }
 
         TaskDTO dto = new TaskDTO();
+        private bool comboFull = false;
 
         private void FrmTask_Load(object sender, EventArgs e)
         {
             label8.Visible = false;
             cmbTaskState.Visible = false;
             dto = TaskBLL.GetAll();
+            dataGridView1.DataSource = dto.employees;
+            dataGridView1.Columns[0].Visible = false;
+            dataGridView1.Columns[1].HeaderText = "User No";
+            dataGridView1.Columns[2].HeaderText = "Name";
+            dataGridView1.Columns[3].HeaderText = "Surname";
+            dataGridView1.Columns[4].Visible = false;
+            dataGridView1.Columns[5].Visible = false;
+            dataGridView1.Columns[6].Visible = false;
+            dataGridView1.Columns[7].Visible = false;
+            dataGridView1.Columns[8].Visible = false;
+            dataGridView1.Columns[9].Visible = false;
+            dataGridView1.Columns[10].Visible = false;
+            dataGridView1.Columns[11].Visible = false;
+            dataGridView1.Columns[12].Visible = false;
+            dataGridView1.Columns[13].Visible = false;
+
+            comboFull = false;
+            cmbDepartment.DataSource = dto.departments;
+            cmbDepartment.DisplayMember = "DepartmentName";
+            cmbDepartment.ValueMember = "ID";
+            cmbPosition.DataSource = dto.positions;
+            cmbPosition.DisplayMember = "PositionName";
+            cmbPosition.ValueMember = "ID";
+            cmbDepartment.SelectedIndex = -1;
+            cmbPosition.SelectedIndex = -1;
+            comboFull = true;
         }
     }
 }
