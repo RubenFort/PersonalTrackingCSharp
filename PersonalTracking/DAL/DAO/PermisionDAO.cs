d606b97919cc552082cec9c13f8f81c726d8a8ac
Ruben <rdfortu@gmail.com>
2021-06-29 17:58:56 +0200

    Listar Permisos => Añado BLL, DTOs, DAO

--- PersonalTracking/DAL/DAO/PermisionDAO.cs
+++ PersonalTracking/DAL/DAO/PermisionDAO.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DAL.DTO;
 
 namespace DAL.DAO
 {
@@ -20,6 +21,38 @@ namespace DAL.DAO
 
                 throw;
             }
+        }
+
+        public static List<PERMISSIONSTATE> GetStates()
+        {
+            return db.PERMISSIONSTATE.ToList();
+        }
+
+        public static List<PermissionDetailDTO> GetPermissions()
+        {
+            List<PermissionDetailDTO> permissions = new List<PermissionDetailDTO>();
+
+            var list = (from p in db.PERMISSION
+                        join s in db.PERMISSIONSTATE on p.PermissionState equals s.ID
+                        join e in db.EMPLOYEE on p.EmployeeID equals e.ID
+                        select new
+                        {
+                            userNo = e.UserNo,
+                            nameof = e.Name,
+                            surname = e.SurName,
+                            stateName = s.StateName,
+                            stateID = p.PermissionState,
+                            startDate = p.PermissionStartDate,
+                            endDate = p.PermissionEndDate,
+                            employeeID = p.EmployeeID,
+                            permissionID = p.ID,
+                            explanationID = p.PermissionExplanation,
+                            dayAmount = p.PermissionDay,
+                            departmentID = e.DepartmentID,
+                            positionID = e.PositionID
+                        }).OrderBy(x => x.startDate).ToList();
+
+            return permissions;
         }
     }
 }
