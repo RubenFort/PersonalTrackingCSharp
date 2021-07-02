b1176a277a69fae916630ac8b4b97933bde273b6
Ruben <rdfortu@gmail.com>
2021-07-02 16:00:10 +0200

    Actualizar salarios => Pasar datso recogidos desde FrmSalaryList a los widgets de FrmSalary

--- PersonalTracking/PersonalTracking/FrmSalary.cs
+++ PersonalTracking/PersonalTracking/FrmSalary.cs
@@ -56,40 +56,53 @@ namespace PersonalTracking
 
         private void FrmSalary_Load(object sender, EventArgs e)
         {
-            dto = SalaryBLL.GetAll();
-            dataGridView1.DataSource = dto.employees;
-            dataGridView1.Columns[0].Visible = false;
-            dataGridView1.Columns[1].HeaderText = "User No";
-            dataGridView1.Columns[2].HeaderText = "Name";
-            dataGridView1.Columns[3].HeaderText = "Surname";
-            dataGridView1.Columns[4].Visible = false;
-            dataGridView1.Columns[5].Visible = false;
-            dataGridView1.Columns[6].Visible = false;
-            dataGridView1.Columns[7].Visible = false;
-            dataGridView1.Columns[8].Visible = false;
-            dataGridView1.Columns[9].Visible = false;
-            dataGridView1.Columns[10].Visible = false;
-            dataGridView1.Columns[11].Visible = false;
-            dataGridView1.Columns[12].Visible = false;
-            dataGridView1.Columns[13].Visible = false;
+            if (!isUpdate)
+            {
+                dto = SalaryBLL.GetAll();
+                dataGridView1.DataSource = dto.employees;
+                dataGridView1.Columns[0].Visible = false;
+                dataGridView1.Columns[1].HeaderText = "User No";
+                dataGridView1.Columns[2].HeaderText = "Name";
+                dataGridView1.Columns[3].HeaderText = "Surname";
+                dataGridView1.Columns[4].Visible = false;
+                dataGridView1.Columns[5].Visible = false;
+                dataGridView1.Columns[6].Visible = false;
+                dataGridView1.Columns[7].Visible = false;
+                dataGridView1.Columns[8].Visible = false;
+                dataGridView1.Columns[9].Visible = false;
+                dataGridView1.Columns[10].Visible = false;
+                dataGridView1.Columns[11].Visible = false;
+                dataGridView1.Columns[12].Visible = false;
+                dataGridView1.Columns[13].Visible = false;
 
-            comboFull = false;
-            cmbDepartment.DataSource = dto.departments;
-            cmbDepartment.DisplayMember = "DepartmentName";
-            cmbDepartment.ValueMember = "ID";
-            cmbPosition.DataSource = dto.positions;
-            cmbPosition.DisplayMember = "PositionName";
-            cmbPosition.ValueMember = "ID";
-            cmbDepartment.SelectedIndex = -1;
-            cmbPosition.SelectedIndex = -1;
+                comboFull = false;
+                cmbDepartment.DataSource = dto.departments;
+                cmbDepartment.DisplayMember = "DepartmentName";
+                cmbDepartment.ValueMember = "ID";
+                cmbPosition.DataSource = dto.positions;
+                cmbPosition.DisplayMember = "PositionName";
+                cmbPosition.ValueMember = "ID";
+                cmbDepartment.SelectedIndex = -1;
+                cmbPosition.SelectedIndex = -1;
 
-            if(dto.departments.Count > 0)
-                comboFull = true;
+                if (dto.departments.Count > 0)
+                    comboFull = true;
+            }
 
             cmbMonth.DataSource = dto.months;//Datos para el ComboBox
             cmbMonth.DisplayMember = "MonthName";//Columna que selecciono para mostrar datos en ComboBox
             cmbMonth.ValueMember = "ID";//Columna que selecciono para coger los datos seleccionados en ComboBox
             cmbMonth.SelectedIndex = -1;
+
+            if (isUpdate)
+            {
+                panel1.Hide();
+                txtName.Text = detail.name;
+                txtSalary.Text = detail.salaryAmount.ToString();
+                txtSurname.Text = detail.surname;
+                txtYear.Text = detail.salaryYear.ToString();
+                cmbMonth.SelectedValue = detail.monthID;
+            }
         }
 
         private void dataGridView1_RowEnter(object sender, DataGridViewCellEventArgs e)
