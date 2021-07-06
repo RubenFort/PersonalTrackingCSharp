e86587aeaa21f1b51465fd24f1b78a05cc76a0d1
Ruben <rdfortu@gmail.com>
2021-07-06 06:32:33 +0200

    Patron Facade en Position. Creo inerface IDAO

---
+++ PersonalTracking/DAL/DAO/IDAO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.DAO
+{
+    interface IDAO<T, K> where T : class where K : class
+    {
+        List<T> Select();
+        bool Insert(K entity);
+        bool Update(K entity);
+        bool Delete(int id);
+    }
+}
--- PersonalTracking/DAL/DAO/PositionDAO.cs
+++ PersonalTracking/DAL/DAO/PositionDAO.cs
@@ -7,7 +7,7 @@ using DAL.DTO;
 
 namespace DAL.DAO
 {
-    public class PositionDAO : EmployeeContext
+    public class PositionDAO : EmployeeContext, IDAO<PositionDTO, POSITION>
     {
         public static void AddPosition(POSITION position)
         {
@@ -18,7 +18,6 @@ namespace DAL.DAO
             }
             catch (Exception)
             {
-                throw;
             }
         }
 
@@ -92,6 +91,82 @@ namespace DAL.DAO
             catch (Exception ex)
             {
                 throw ex;
+            }
+        }
+
+        public List<PositionDTO> Select()
+        {
+            try
+            {
+                var list = (from p in db.POSITION
+                            join d in db.DEPARTMENT on p.DepartmentID equals d.ID
+                            select new
+                            {
+                                positionID = p.ID,
+                                positionName = p.PositionName,
+                                departmentName = d.DepartmentName,
+                                departmentID = p.DepartmentID
+                            }).OrderBy(x => x.positionID).ToList();
+                List<PositionDTO> positionList = new List<PositionDTO>();
+                foreach (var item in list)
+                {
+                    PositionDTO dto = new PositionDTO();
+                    dto.ID = item.positionID;
+                    dto.PositionName = item.positionName;
+                    dto.departmentNane = item.departmentName;
+                    dto.DepartmentID = item.departmentID;
+                    positionList.Add(dto);
+                }
+                return positionList;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public bool Insert(POSITION entity)
+        {
+            try
+            {
+                db.POSITION.InsertOnSubmit(entity);
+                db.SubmitChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool Update(POSITION entity)
+        {
+            try
+            {
+                POSITION pst = db.POSITION.First(x => x.ID == entity.ID);
+                pst.PositionName = entity.PositionName;
+                pst.DepartmentID = entity.DepartmentID;
+                db.SubmitChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public bool Delete(int id)
+        {
+            try
+            {
+                POSITION position = db.POSITION.First(x => x.ID == id);
+                db.POSITION.DeleteOnSubmit(position);
+                db.SubmitChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
             }
         }
     }
